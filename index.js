f282c01cf09a3373b082222ab0f65adb089557f2
asur1357 <sukanil1357@gmail.com>
2025-06-15 03:33:58 +0530

    🚀origin url added

--- index.js
+++ index.js
@@ -12,6 +12,14 @@ const app = express()
 const port = process.env.PORT || 3000
 
 
+const corsOptions = {
+    origin: ['https://snappy1357.vercel.app'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true,
+}
+
+app.use(cors(corsOptions))
+
 app.use(cors())
 app.use(express.json())
 app.use(bodyParser.urlencoded({extended: true}))
